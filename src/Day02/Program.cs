2485b3b72c5a70ec19e472aeb3b24f820cac5fe9
Cain Voong <cainux@gmail.com>
2023-12-02 16:21:48 +0000

    Day 2 Part 2 🎄

--- src/Day02/Program.cs
+++ src/Day02/Program.cs
@@ -2,35 +2,36 @@
 
 // Part 01
 var constraint = ParseHand("12 red, 13 green, 14 blue");
-
 Console.WriteLine(input.Sum(x => Test(constraint, x)));
 
+// Part 02
+Console.WriteLine(input.Sum(x => Power(ParseGame(x))));
 return;
 
-short Test(Hand c, string gameLine)
+int Test(Hand c, string gameLine)
 {
     var g = ParseGame(gameLine);
-    return g.Hands.Any(h => h.Red > c.Red || h.Green > c.Green || h.Blue > c.Blue) ? (short) 0 : g.Number;
+    return g.Hands.Any(h => h.Red > c.Red || h.Green > c.Green || h.Blue > c.Blue) ? 0 : g.Number;
 }
 
 Game ParseGame(string gameLine)
 {
     var parts = gameLine.Split(':');
-    var number = short.Parse(parts[0].Split(' ')[1]);
+    var number = int.Parse(parts[0].Split(' ')[1]);
     var hands = parts[1].Split(';').Select(x => x.Trim()).Select(ParseHand).ToArray();
     return new Game(number, hands);
 }
 
 Hand ParseHand(string handLine)
 {
-    short r = 0, g = 0, b = 0;
+    int r = 0, g = 0, b = 0;
     var hands = handLine.Split(',').Select(x => x.Trim()).ToArray();
 
     foreach (var set in hands)
     {
         var parts = set.Split(' ');
         var colour = parts[1];
-        var count = short.Parse(parts[0]);
+        var count = int.Parse(parts[0]);
 
         switch (colour)
         {
@@ -51,6 +52,26 @@ Hand ParseHand(string handLine)
     return new Hand(r, g, b);
 }
 
-internal record Game(short Number, Hand[] Hands);
+int Power(Game game)
+{
+    var h = MinSet(game.Hands);
+    return h.Red * h.Green * h.Blue;
+}
 
-internal record Hand(short Red, short Green, short Blue);
+Hand MinSet(Hand[] hands)
+{
+    int r = 0, g = 0, b = 0;
+
+    foreach (var hand in hands)
+    {
+        r = Math.Max(r, hand.Red);
+        g = Math.Max(g, hand.Green);
+        b = Math.Max(b, hand.Blue);
+    }
+
+    return new Hand(r, g, b);
+}
+
+internal record Game(int Number, Hand[] Hands);
+
+internal record Hand(int Red, int Green, int Blue);
