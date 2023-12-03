3408960f82b2557901091dfa9221991cb9a76323
Cain Voong <cainux@gmail.com>
2023-12-03 11:14:35 +0000

    Day 3 Part 1

---
+++ src/Day03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+const char period = '.';
+var lines = File.ReadAllLines("input.txt");
+var maxX = lines[0].Length - 1;
+var maxY = lines.Length - 1;
+var acc = 0;
+
+for (var y = 0; y < lines.Length; y++)
+    acc += Regex.Matches(lines[y], @"\d+").Sum(m => IsPartNumber(m.Index, y, m.Length) ? int.Parse(m.Value) : 0);
+
+Console.WriteLine(acc);
+
+return;
+
+bool IsPartNumber(int x, int y, int length)
+{
+    if (x > 0 && lines[y][x - 1] != period) return true;
+    if (x + length < maxX && lines[y][x + length] != period) return true;
+
+    if (y > 0)
+    {
+        var lineAbove = lines[y - 1];
+        var start = Math.Max(0, x - 1);
+        var end = Math.Min(maxX, x + length);
+        for (var i = start; i <= end; i++)
+            if (lineAbove[i] != period) return true;
+    }
+
+    if (y + 1 < maxY)
+    {
+        var lineBelow = lines[y + 1];
+        var start = Math.Max(0, x - 1);
+        var end = Math.Min(maxX, x + length);
+        for (var i = start; i <= end; i++)
+            if (lineBelow[i] != period) return true;
+    }
+
+    return false;
+}
