9c62dc2c2e344f66c0b5deaa8b6ab7adcd5fcc0f
Cain Voong <cainux@gmail.com>
2023-12-09 18:17:37 +0000

    Day 6 Part 2

--- src/Day06/Program.cs
+++ src/Day06/Program.cs
@@ -10,28 +10,35 @@ var part1 = 1;
 
 for (var i = 0; i < raceCount; i++)
 {
-    var race = i + 1;
-    var time = times[i];
-    var distance = distances[i];
-
-    Console.WriteLine($"Race {race}: {time} {distance}");
-
+    var t = times[i];
+    var d = distances[i];
     var acc = 0;
 
-    for (var x = 1; x < time; x++)
+    for (var x = 1; x < t; x++)
     {
-        var travelled = x * (time - x);
-        if (travelled > distance)
-        {
-            Console.WriteLine($"  {x:000}: {travelled:000}");
+        var travelled = x * (t - x);
+        if (travelled > d)
             acc++;
-        }
     }
 
     part1 *= acc;
 }
 
 Console.WriteLine(part1);
+
+// Part 2
+var time = ulong.Parse(string.Join("", times.Select(x => x.ToString())));
+var distance = ulong.Parse(string.Join("", distances.Select(x => x.ToString())));
+
+var c = 0;
+for (var x = 1UL; x < time; x++)
+{
+    var travelled = x * (time - x);
+    if (travelled > distance)
+        c++;
+}
+
+Console.WriteLine(c);
 
 return;
 
