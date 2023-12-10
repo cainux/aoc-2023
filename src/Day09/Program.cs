35abf8957858bbf9e4a7b98629b4ecf30e2fd3b6
Cain Voong <cainux@gmail.com>
2023-12-10 21:57:03 +0000

    Day 9 Part 2

--- src/Day09/Program.cs
+++ src/Day09/Program.cs
@@ -1,3 +1,6 @@
 ﻿var lines = File.ReadAllLines("input.txt").Select(x => x.Split(' ').Select(int.Parse).ToArray()).ToArray();
-Console.WriteLine(lines.Sum(Predict));
-int Predict(int[] input) => input.All(x => x == 0) ? 0 : input[^1] + Predict(input.Zip(input[1..]).Select(x => x.Second - x.First).ToArray());
+Console.WriteLine(lines.Sum(Part1));
+int Part1(int[] input) => input.All(x => x == 0) ? 0 : input[^1] + Part1(input.Zip(input[1..]).Select(x => x.Second - x.First).ToArray());
+
+Console.WriteLine(lines.Sum(Part2));
+int Part2(int[] input) => input.All(x => x == 0) ? 0 : input[0] - Part2(input.Zip(input[1..]).Select(x => x.Second - x.First).ToArray());
